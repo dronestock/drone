c48c259f1468db20bc3b51b258cebb7eb3865e1e
Storezhang <storezhang@gmail.com>
2022-01-18 21:47:09 +0800

    抽象步骤来规范插件的开发

--- bootstrap.go
+++ bootstrap.go
@@ -2,6 +2,7 @@ package drone
 
 import (
 	`fmt`
+	`os`
 	`sync`
 	`time`
 
@@ -67,6 +68,11 @@ func Bootstrap(plugin plugin, opts ...option) (err error) {
 		logger.Error(fmt.Sprintf(`%s插件执行出错，请检查`, _options.name))
 	} else {
 		logger.Info(fmt.Sprintf(`%s插件执行成功，恭喜`, _options.name))
+
+		// 退出程序，解决最外层panic报错的问题
+		// 原理：如果到这个地方还没有发生错误，程序正常退出，外层panic得不到执行
+		// 如果发生错误，则所有代码都会返回error直到panic检测到，然后程序整体panic
+		os.Exit(0)
 	}
 
 	return
