9149e6ca81975341788a94cef9309e4794777875
storezhang <storezhang@gmail.com>
2023-01-17 16:57:43 +0800

    feat(插件): 将环境变量处理器重新设计

--- bootstrap_processor.go
+++ bootstrap_processor.go
@@ -7,15 +7,13 @@ import (
 	"strings"
 )
 
-type sliceProcessor struct{}
-
-func (sp *sliceProcessor) Process(tag string, field reflect.StructField) (to string, err error) {
+func (b *bootstrap) Process(tag string, field reflect.StructField) (to string, err error) {
 	to = tag
-	if !sp.canConvert(tag, field) {
+	if !b.canConvert(tag, field) {
 		return
 	}
 
-	separator := `,`
+	separator := ","
 	values := strings.Split(tag, separator)
 	finals := make([]string, 0, len(values))
 	for _, value := range values {
@@ -25,13 +23,13 @@ func (sp *sliceProcessor) Process(tag string, field reflect.StructField) (to str
 			finals = append(finals, fmt.Sprintf(`"%s"`, value))
 		}
 	}
-	to = fmt.Sprintf(`[%s]`, strings.Join(finals, separator))
+	to = fmt.Sprintf("[%s]", strings.Join(finals, separator))
 
 	return
 }
 
-func (sp *sliceProcessor) canConvert(from string, field reflect.StructField) bool {
-	return `` != strings.TrimSpace(from) && // 不能是空字符串
+func (b *bootstrap) canConvert(from string, field reflect.StructField) bool {
+	return "" != strings.TrimSpace(from) && // 不能是空字符串
 		reflect.Slice == field.Type.Kind() && // 只能是列表
 		!(strings.HasPrefix(from, jsonArrayStart) && strings.HasSuffix(from, jsonArrayEnd)) // 不能是数组
 }
