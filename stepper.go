12e966f443bb7676229a2bf6450632b0ecad440d
storezhang <storezhang@gmail.com>
2023-01-17 11:19:55 +0800

    feat(步骤): 增加执行上下文

--- stepper.go
+++ stepper.go
@@ -1,8 +1,13 @@
 package drone
 
+import (
+	"context"
+)
+
 type stepper interface {
 	// Runnable 是否执行步骤
 	Runnable() bool
+
 	// Run 执行步骤
-	Run() (err error)
+	Run(ctx context.Context) (err error)
 }
