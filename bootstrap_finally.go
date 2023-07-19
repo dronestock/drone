60197c96c16fb7757c345791f9765a9cf00eede0
storezhang <storezhang@gmail.com>
2023-07-19 12:28:16 +0800

    feat(插件): 最终步骤引入recover机制打印出调用堆栈

--- bootstrap_finally.go
+++ bootstrap_finally.go
@@ -1,14 +1,23 @@
 package drone
 
 import (
+	"fmt"
 	"os"
+	"runtime"
+	"strings"
 	"time"
 
+	"github.com/dronestock/drone/internal"
 	"github.com/goexl/gox"
 	"github.com/goexl/gox/field"
 )
 
 func (b *bootstrap) finally(err *error) {
+	if finally := recover(); nil != finally {
+		fmt.Println(b.stack())
+		os.Exit(internal.ExitCodeFailed)
+	}
+
 	if code, ee := b.exec(); nil != ee {
 		b.Warn("执行命令出错", field.New("code", code), field.New("commands", b.Commands), field.Error(ee))
 	} else {
@@ -25,8 +34,27 @@ func (b *bootstrap) finally(err *error) {
 	}
 	b.Info("插件执行完成", fields...)
 	if nil == *err {
-		os.Exit(0)
+		os.Exit(internal.ExitCodeOk)
 	} else {
-		os.Exit(1)
+		os.Exit(internal.ExitCodeFailed)
 	}
+}
+
+func (b *bootstrap) stack() string {
+	stack := 10
+	skip := 4
+	callers := make([]uintptr, stack+1)
+	count := runtime.Callers(skip+2, callers)
+	frames := runtime.CallersFrames(callers[:count])
+
+	stacks := make([]string, 0, stack)
+	for {
+		frame, more := frames.Next()
+		stacks = append(stacks, fmt.Sprintf("%s()\n\t%s:%d", frame.Function, frame.File, frame.Line))
+		if !more {
+			break
+		}
+	}
+
+	return strings.Join(stacks, "\n")
 }
