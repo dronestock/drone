c0f62373afe60e8ce7b0da7999b2e772189c103a
storezhang <storezhang@gmail.com>
2024-01-17 10:18:23 +0800

    feat(插件): 增加获取值

--- internal/internal/value.go
+++ internal/internal/value.go
@@ -1,5 +1,10 @@
 package internal
 
+import (
+	"strconv"
+	"time"
+)
+
 type Value struct {
 	content string
 }
@@ -12,4 +17,14 @@ func NewValue(content string) *Value {
 
 func (v *Value) String() string {
 	return v.content
+}
+
+func (v *Value) Timestamp() (stamp string) {
+	if created, pie := strconv.ParseInt(v.content, 10, 64); nil == pie {
+		stamp = time.Unix(created, 0).Format(time.RFC3339)
+	} else {
+		stamp = time.Now().Format(time.RFC3339)
+	}
+
+	return
 }
